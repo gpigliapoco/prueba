d3eb5ed3eb2c238cc15bf1389a1210fda876770e
leo <gpigliapoco@gmail.com>
2021-06-07 00:07:09 -0300

    prueba

--- controlador/controladorUsu.php
+++ controlador/controladorUsu.php
@@ -5,9 +5,9 @@
 	$mu= new modelo_usuario();
 	$usu = (isset($_POST['usu'])) ? $_POST['usu'] : '';
     $pass = (isset($_POST['pass'])) ? $_POST['pass'] : '';
-    printf($usu);
+    
     $consulta=$mu->agregarUsuario($usu, $pass);
-    echo $consulta;
+    
     
 
  ?>
