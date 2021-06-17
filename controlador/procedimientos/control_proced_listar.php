c5ec425e872188694f1ea7ff872dc005d96087e8
gerardo <gpigliapoco@gmail.com>
2021-06-17 20:35:50 -0300

    procedimientos

---
+++ controlador/procedimientos/control_proced_listar.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../modelo/m_procedimientos.php';
+
+$mu= new modelo_procedimientos();
+$consulta=$mu->listar_procedimientos();
+if($consulta){
+    echo json_encode($consulta);
+}else{
+    echo '{
+        "sEcho": 1,
+        "iTotalRecords": "0",
+        "iTotalDisplayRecords": "0",
+        "aaData": []
+    }';
+    
+}
+
+?>
