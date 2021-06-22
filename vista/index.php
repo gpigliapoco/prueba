29741e244eabd3ae087e1f0f234fd2147f5efe01
gerardo <gpigliapoco@gmail.com>
2021-06-22 10:39:01 -0300

    FUNCION SOLO NUMERO Y LETARS

--- vista/index.php
+++ vista/index.php
@@ -498,6 +498,33 @@ if(!isset($_SESSION['S_idusuario'])){
     $("#"+contenido).load(contenedor);
   }
 
+  function soloNumeros(e){
+      tecla = (document.all) ? e.keyCode : e.which;
+      if (tecla==8){
+          return true;
+      }
+      // Patron de entrada, en este caso solo acepta numeros
+      patron =/[0-9]/;
+      tecla_final = String.fromCharCode(tecla);
+      return patron.test(tecla_final);
+  }
+  function soloLetras(e){
+      key = e.keyCode || e.which;
+      tecla = String.fromCharCode(key).toLowerCase();
+      letras = "áéíóúabcdefghijklmnñopqrstuvwxyz";
+      especiales = "8-37-39-46";
+      tecla_especial = false
+      for(var i in especiales){
+          if(key == especiales[i]){
+              tecla_especial = true;
+              break;
+          }
+      }
+      if(letras.indexOf(tecla)==-1 && !tecla_especial){
+          return false;
+      }
+  }
+
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
